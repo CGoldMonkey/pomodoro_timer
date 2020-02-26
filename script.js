baabab93e86c558a9ad6e37783a210af46ceac59
asteven <20649579+CGoldMonkey@users.noreply.github.com>
2020-02-26 12:35:26 -0600

    Refactored the code for setting the session/break time using the buttons

--- script.js
+++ script.js
@@ -1,43 +1,15 @@
-const sessionButtons = document.querySelectorAll('#session button');
-const sessionDisplay = document.querySelector('#session span')
-const breakButtons = document.querySelectorAll('#break button');
-const breakDisplay = document.querySelector('#break span')
-
 const settingButtons = document.querySelectorAll('.settings button')
 
-sessionDisplay.textContent = '25';
-breakDisplay.textContent = '5';
-
-
-sessionButtons.forEach(button => {
+settingButtons.forEach(button => {
   button.addEventListener('click', (e) => {
-    console.log(e)
-    console.log(e.target)
-    console.log(e.target.parentNode.id)
-    if (e.target.parentNode.id === 'session') {
-      console.log('Parent Here')
-    }
-    let currentNumber = parseInt(sessionDisplay.textContent);
-    if (e.target.textContent === '+') {
-      currentNumber++
-      sessionDisplay.textContent = currentNumber;
-    } else {
-      currentNumber--
-      sessionDisplay.textContent = currentNumber;
-    }
-  })
-})
-//all the function needs is the display and button to be passed to it
-//, the
-breakButtons.forEach(button => {
-  button.addEventListener('click', (e) => {
-    let currentNumber = parseInt(breakDisplay.textContent);
+    let display = e.target.parentNode.querySelector('.display');
+    let currentNumber = parseInt(display.textContent);
+
     if (e.target.textContent === '+') {
       currentNumber++;
-      breakDisplay.textContent = currentNumber;
     } else {
-      currentNumber--;
-      breakDisplay.textContent = currentNumber;
+      currentNumber--;      
     }
+    display.textContent = currentNumber;
   })
 })
